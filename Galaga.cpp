4863172158e03ad664d8c56d709ccb4f0d0cf0e7
YeongHun <rkddudgns98@naver.com>
2020-01-01 21:59:23 +0900

    Galaga: add elements of game and rebuild with function

--- Galaga.cpp
+++ Galaga.cpp
@@ -4,88 +4,124 @@
 
 #include "Console.h"
 
-#define MAX 30
+#define MAX 40
+#define WIDTH 50
+#define LENGTH 50
 
-struct ST_ENEMY {	//적 구조체
+int SCORE = 0;
+
+struct ST_OBJECT {	//적 구조체
 	int x = 0;
 	int y = 0;
 	bool bActive = false;
 };
 
+ST_OBJECT g_stEnemy[MAX];
+ST_OBJECT g_stPlayer;
+
+void Init() {
+	g_stPlayer.x = WIDTH - 2, g_stPlayer.y = LENGTH - 2;	//처음 비행기좌표
+	g_stPlayer.bActive = true;	//목숨 on
+	srand(time(NULL));
+	SetConsoleSize(WIDTH, LENGTH);	//콘솔창 사이즈 조절
+}
+
+void Spawn() {
+	for (int i = 0; i < MAX; i++) {	//적 수 늘리기
+		if (!g_stEnemy[i].bActive) {
+			g_stEnemy[i].x = (rand() % (WIDTH / 2)) * 2;	//적 위치 랜덤생성
+			g_stEnemy[i].y = 0;
+			g_stEnemy[i].bActive = true;
+			break;
+		}
+	}
+}		//
+
+void InputProcess() {
+	if (GetAsyncKeyState(VK_LEFT) & 0x8000) {	//왼쪽방향키
+		g_stPlayer.x = g_stPlayer.x - 2;
+		if (g_stPlayer.x < 0) g_stPlayer.x = 0;
+	}
+
+	if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {	//오른쪽방향키
+		g_stPlayer.x = g_stPlayer.x + 2;
+		if (g_stPlayer.x > WIDTH - 2) g_stPlayer.x = WIDTH - 2;
+	}
+
+	if (GetAsyncKeyState(VK_UP) & 0x8000) {	//위쪽방향키
+		g_stPlayer.y--;
+		if (g_stPlayer.y < 0) g_stPlayer.y = 0;
+	}
+
+	if (GetAsyncKeyState(VK_DOWN) & 0x8000) {	//아래쪽방향키
+		g_stPlayer.y++;
+		if (g_stPlayer.y > LENGTH - 2) g_stPlayer.y = LENGTH - 2;
+	}
+}
+
+int EnemyProcess() {
+	int count = 0;
+	for (int i = 0; i < MAX; i++) {
+		if (g_stEnemy[i].bActive) {	//적기
+			count++;
+			GotoXY(g_stEnemy[i].x, g_stEnemy[i].y);
+			printf("■");
+			g_stEnemy[i].y++;
+
+			if (g_stEnemy[i].y == g_stPlayer.y) {	//충돌
+				if (g_stEnemy[i].x == g_stPlayer.x)	//(x,y)좌표 같을 때
+					g_stPlayer.bActive = false;	//목숨 off
+			}
+
+			if (g_stEnemy[i].y > LENGTH - 2) g_stEnemy[i].bActive = false;	//적기가 벗어나면 삭제
+		}
+	}
+
+	return count;
+}
+
+void Update() {
+	if (g_stPlayer.bActive) {
+		GotoXY(g_stPlayer.x, g_stPlayer.y);	//커서이동
+		printf("▲");	//비행기 출력
+	}
+	else {
+		GotoXY(g_stPlayer.x, g_stPlayer.y);	//커서이동
+		printf("▒");	//비행기 출력
+	}
+}
+
+void Score() {
+	if(g_stPlayer.bActive)	SCORE++;
+	GotoXY(0, 0);
+	printf("SCORE: %d", SCORE);
+}
+
 int main() {
 
-	int width = 30, length = 30;	//콘솔 사이즈
+	Init();	//initialize
 
-	int x = width - 2, y = length - 2;	//처음 비행기좌표
-
-	int bullet = 0, bx = 0, by = 0;	//총알의 좌표
-
-	ST_ENEMY stEnemy[MAX];
-
-	srand(time(NULL));
-	
-	SetConsoleSize(width, length);	//콘솔창 사이즈 조절
+	int state = 0;	//게임상태 on off
 
 	while (1) {
 		Clear();	//잔상 지우기
 
-		for (int i = 0; i < MAX; i++) {	//적 수 늘리기
-			if (!stEnemy[i].bActive) {
-				stEnemy[i].x = (rand() % (width / 2)) * 2;	//적 위치 랜덤생성
-				stEnemy[i].y = 0;
-				stEnemy[i].bActive = true;
-				break;
-			}
+		if (g_stPlayer.bActive) {	//목숨 on일때
+			Spawn();	//적 생성
+
+			InputProcess();	//이동키
+
 		}
 
-		if (GetAsyncKeyState(VK_LEFT) & 0x8000) {	//왼쪽방향키
-			x--;
-			if (x < 0) x = 0;
-		}
+		state = EnemyProcess();	//적 이동
 
-		if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {	//오른쪽방향키
-			x++;
-			if (x > width - 2) x = width - 2;
-		}
+		Score();	//점수
 
-		if (GetAsyncKeyState(VK_UP) & 0x8000) {	//위쪽방향키
-			y--;
-			if (y < 0) y = 0;
-		}
-
-		if (GetAsyncKeyState(VK_DOWN) & 0x8000) {	//아래쪽방향키
-			y++;
-			if (y > length - 2) y = length - 2;
-		}
-
-		if (GetAsyncKeyState(VK_SPACE) & 0x8000) {	//스페이스바
-			if (!bullet) {	//총알이 없을때
-				bx = x;		//총알의 x좌표
-				by = y - 1;	//총알의 y좌표
-				bullet = true;
-			}
-		}
-
-		if (bullet) {	//총알쏘기
-			GotoXY(bx, by);	//커서이동
-			printf("＊");	//총알쏘기
-			by--;	//총알 이동
-			if (by < 0) bullet = false;	//화면을 벗어났을때 총알지우기
-		}
-
-		for (int i = 0; i < MAX; i++) {
-			if (stEnemy[i].bActive) {	//적기
-				GotoXY(stEnemy[i].x, stEnemy[i].y);
-				printf("■");
-				stEnemy[i].y++;
-				if (stEnemy[i].y > length - 2) stEnemy[i].bActive = false;
-			}
-		}
-
-		GotoXY(x, y);	//커서이동
-		printf("▲");	//비행기 출력
+		Update();	//플레이어 생성
 
 		Sleep(50);	//텀
+
+		if (state == 0) break;	//죽어서 적 이동이 0이 되면 게임 끝
 	}
 
 	system("pause");
