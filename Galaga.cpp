2de6f18bfe0bfe15637c0d55c254a635823452f1
YeongHun <rkddudgns98@naver.com>
2020-01-02 16:45:00 +0900

    Galaga: add code bullet(one shot) and upload Galaga.exe, Console.h and Console.cpp

--- Galaga.cpp
+++ Galaga.cpp
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <conio.h>
+
 #include "Console.h"
 
-#define MAX 30
-#define WIDTH 40		//4의 배수로 하세요
+#define MAX 50	//적기의 개수 = 난이도
+#define WIDTH 60		//4의 배수로 하세요
 #define LENGTH 40		
 #define BACKCOLOR 0
 #define OBJECTCOLOR 15
 
 int SCORE = 0;
+int bullet = 0, bx = 0, by = 0;
 
 struct ST_OBJECT {	//적 구조체
 	int x = 0;
@@ -30,6 +32,7 @@ void Init() {
 }
 
 void Spawn() {
+	
 	for (int i = 0; i < MAX; i++) {	//적 수 늘리기
 		if (!g_stEnemy[i].bActive) {
 			g_stEnemy[i].x = (rand() % (WIDTH / 2)) * 2;	//적 위치 랜덤생성
@@ -42,6 +45,7 @@ void Spawn() {
 
 int EnemyProcess() {
 	int count = 0;
+	
 	for (int i = 0; i < MAX; i++) {
 		if (g_stEnemy[i].bActive) {	//적기
 			SetColor(BACKCOLOR, i % 15);
@@ -49,6 +53,15 @@ int EnemyProcess() {
 			GotoXY(g_stEnemy[i].x, g_stEnemy[i].y);
 			printf("☆");
 			g_stEnemy[i].y++;
+
+			if (bx == g_stEnemy[i].x) {	//적기가 총알을 맞았을때
+				if (by <= g_stEnemy[i].y) {
+					g_stEnemy[i].bActive = false;	//적기 목숨 off
+					bullet = false;
+					bx = g_stPlayer.x;		//총알의 x좌표
+					by = g_stPlayer.y;		//총알의 y좌표
+				}
+			}
 
 			if (g_stEnemy[i].y == g_stPlayer.y) {	//충돌
 				if (g_stEnemy[i].x == g_stPlayer.x)	//(x,y)좌표 같을 때
@@ -107,10 +120,14 @@ void StartMenu() {
 
 	while (1) {
 		Clear();
-		GotoXY(WIDTH / 4, LENGTH / 3);
-		printf("Galaga");
-		GotoXY(WIDTH / 4, LENGTH / 2);
-		printf("Press any key");
+		GotoXY(2, LENGTH / 5);
+		printf("#Galaga");
+		GotoXY(2, LENGTH / 4);
+		printf("#조작:←↑↓→ 총알:SPACEBAR");
+		GotoXY(2, LENGTH / 3);
+		printf("#주의: 한번에 하나의 총알만 발사 할 수 있습니다");
+		GotoXY(2, LENGTH / 2);
+		printf("#Press any key");
 		Sleep(50);
 		if (_kbhit()) break;
 	}
@@ -145,6 +162,34 @@ bool ResultMenu() {
 	return bRet;
 }
 
+void Bullet() {
+	if (GetAsyncKeyState(VK_SPACE) & 0x8000) {	//스페이스바
+		if (!bullet) {	//총알이 없을때
+			bx = g_stPlayer.x;		//총알의 x좌표
+			by = g_stPlayer.y;	//총알의 y좌표
+			bullet = true;
+		}
+	}
+
+	if (bullet) {	//총알쏘기
+		GotoXY(bx, by);	//커서이동
+		
+		SetColor(BACKCOLOR, rand() % 15);
+		
+		printf("＊");	//총알쏘기
+		by--;	//총알 이동
+
+		if (g_stPlayer.bActive == false) {
+			bx = g_stPlayer.x;		//총알의 x좌표
+			by = g_stPlayer.y;		//총알의 y좌표 ;
+			bullet = false;
+		}
+
+
+		if (by < 0) bullet = false;	//화면을 벗어났을때 총알지우기
+	}
+}
+
 void GameMain() {
 	int state = 0;	//게임상태 on off
 
@@ -163,18 +208,19 @@ void GameMain() {
 			Spawn();	//적 생성
 
 			InputProcess();	//이동키
-
 		}
-
+		
+		Bullet();	//총알
+		
 		state = EnemyProcess();	//적 이동
 
 		Score();	//점수
 
-		Update();	//플레이어 생성
+		Update();	//플레이어 위치, 상태
 
 		Sleep(50);	//텀
 
-		if (state == 0) break;	//죽어서 적 이동이 0이 되면 게임 끝
+		if (state == 0) break;	//플레이어가 죽어서 적 이동이 0이 되면 게임 끝
 	}
 }
 
